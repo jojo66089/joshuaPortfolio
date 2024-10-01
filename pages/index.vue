18e7d0a8269ad18bc18b76de2cb5110fcd4e72ca
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2024-10-01 19:51:55 -0400

    added effects and developped skeleton for home page

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="image-container">
+    <div id= "header"><img src= "/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 3.png">
+      <div class="button-container">
+        <button type="projects">
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 5.png" alt="buttonpng" border="0" />
+        </button>
+        <button type="resume">
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 9.png" alt="buttonpng" border="0" />
+        </button>
+        <button type="contact">
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 7.png" alt="buttonpng" border="0" />
+        </button>
+      </div>
+      <div class ="image-container2" >
+        <button class="button"><img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 12.png"></button>
+        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
+        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
+        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
+      </div>
+  </div>
+    
+  </div>
+</template>
+
+
