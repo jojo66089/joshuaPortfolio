e4d4f33f56d6e79ac71c88caa11a1db255b329a3
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2024-10-02 17:34:28 -0400

    added phone scalings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css']
+  
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })
