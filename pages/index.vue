6f6b3ff564d59db193636ccf45e05ef5528df942
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2024-10-02 14:08:27 -0400

    created FULLY FUNCTIONNAL skeleton for main page

--- pages/index.vue
+++ pages/index.vue
@@ -5,29 +5,28 @@ const route = useRoute()
 <template>
   <div class="image-container">
     <div id="header">
-      <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 3.png">
-      
+      <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/1x/1x/Asset 24.png">
       <div class="button-container">
     
         <div class="button-row-container">
           <button class="projects-button">
             <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 5.png" alt="buttonpng" border="0" />
           </button>
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 11.png" alt="buttonpng" border="0" />
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/1x/Asset 17.png" alt="buttonpng" border="0" />
         </div>
 
         <div class="button-row-container">
           <button class="resume-button">
             <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 9.png" alt="buttonpng" border="0" />
           </button>
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 12.png" alt="buttonpng" border="0" />
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/1x/Asset 20.png" alt="buttonpng" border="0" />
         </div>
 
         <div class="button-row-container">
           <button class="contact-button">
             <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 7.png" alt="buttonpng" border="0" />
           </button>
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 13.png" alt="buttonpng" border="0" />
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/1x/Asset 21.png" alt="buttonpng" border="0" />
         </div>
       </div>
     </div>
