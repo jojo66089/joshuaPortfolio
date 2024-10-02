c3be89dcd5c852c187926e27a940cabde381a4d2
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2024-10-01 20:13:28 -0400

    fully functional strobe light css effect for buttons, still needs new images tho

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,32 @@ const route = useRoute()
 
 <template>
   <div class="image-container">
-    <div id= "header"><img src= "/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 3.png">
+    <div id="header">
+      <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 3.png">
+      
       <div class="button-container">
-        <button type="projects">
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 5.png" alt="buttonpng" border="0" />
-        </button>
-        <button type="resume">
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 9.png" alt="buttonpng" border="0" />
-        </button>
-        <button type="contact">
-          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 7.png" alt="buttonpng" border="0" />
-        </button>
-      </div>
-      <div class ="image-container2" >
-        <button class="button"><img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 12.png"></button>
-        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
-        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
-        <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 10.png">
-      </div>
-  </div>
     
+        <div class="button-row-container">
+          <button class="projects-button">
+            <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 5.png" alt="buttonpng" border="0" />
+          </button>
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 11.png" alt="buttonpng" border="0" />
+        </div>
+
+        <div class="button-row-container">
+          <button class="resume-button">
+            <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 9.png" alt="buttonpng" border="0" />
+          </button>
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 12.png" alt="buttonpng" border="0" />
+        </div>
+
+        <div class="button-row-container">
+          <button class="contact-button">
+            <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 7.png" alt="buttonpng" border="0" />
+          </button>
+          <img src="/Users/joshuaguillen/Desktop/website_portfolio/joshuaPortfolio/joshuaPortfolio/1xx/1x/Asset 13.png" alt="buttonpng" border="0" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-
