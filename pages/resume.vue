60da24b94baf917395a8d2e11e1b3c2ada7ea17f
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2024-10-18 15:37:20 -0400

    fixed html redirect to pdf

--- pages/resume.vue
+++ pages/resume.vue
@@ -108,7 +108,7 @@ const route = useRoute();
 
     <!-- Button to Download Resume as PDF -->
     <div class="download-button-container">
-        <a href="/path/to/Joshua Guillen Resume.pdf" download class="download-button">Download PDF</a>
+        <a href="~/assets/resume/Joshua Guillen Resume.pdf" download class="download-button">Download PDF</a>
     </div>
 </div>
 <HomeButton />
