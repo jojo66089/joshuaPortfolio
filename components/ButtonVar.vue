3e336c5e19ce550a95109ba682927d58d6ea3086
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2025-04-24 02:34:38 -0400

    motions and redesign

---
+++ components/ButtonVar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="image-container">
+        <div class="button-container">
+            <!-- Projects Button -->
+            <div class="button-row-container-bar">
+                <NuxtLink to="/projects" class="projects-button">
+                    <img src="/assets/1xx/1x/1x/Asset 17.png" alt="buttonpng" border="0" />
+                    <img src="/assets/1xx/1x/Asset 5.png" alt="buttonpng" border="0" />
+                </NuxtLink>
+            </div>
+
+            <!-- Resume Button -->
+            <div class="button-row-container-bar">
+                <NuxtLink to="/resume" class="resume-button">
+                    <img src="/assets/1xx/1x/1x/Asset 20.png" alt="buttonpng" border="0" />
+                    <img src="/assets/1xx/1x/Asset 9.png" alt="buttonpng" border="0" />
+                </NuxtLink>
+            </div>
+
+            <!-- Contact Button -->
+            <div class="button-row-container-bar">
+                <NuxtLink to="/contact" class="contact-button3">
+                    <img src="/assets/1xx/1x/1x/Asset 21.png" alt="buttonpng" border="0" />
+                    <img src="/assets/1xx/1x/Asset 7.png" alt="buttonpng" border="0" />
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.button-row-container-bar {
+  display: flex;
+  flex-direction: row; /* Arrange images in a row */
+  align-items: center;
+  gap: 200px;
+}
+
+.button-row-container-bar .projects-button,
+.button-row-container-bar .resume-button,
+.button-row-container-bar .contact-button3 {
+  display: flex;
+  flex-direction: row; 
+  align-items: center;
+}
+
+.button-row-container-bar img {
+  display: block; 
+  margin: 0;
+  padding: 0;
+  max-width: 70px;
+  max-height: 70px;
+  transition: 0.3s ease-in-out;
+}
+
+.button-row-container-bar:hover img {
+  animation: strobe 0.5s infinite alternate;
+  filter: brightness(0) invert(1);
+}
+
+</style>
+
---
+++ components/headerTitle.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="intro">
+      <div class="right">
+        <img src="~assets/1xx/1x/1x/1x/1x/Asset 26.png" alt="">
+
+      </div>
+      <div class="logo">
+        <img src="~assets/1xx/1x/1x/1x/1x/Asset 3.png" alt="">
+      </div>
+
+    
+      <div class="left">
+        <img src="~assets/1xx/1x/1x/1x/1x/Asset 27.png" alt="">
+      </div>
+
+    </div>
+</template>
+<style scoped>
+.intro {
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  max-width: auto;
+  max-height: auto;
+  overflow: visible;
+  transform: scale(3) translateY(50%);
+  transition-duration: 0.6s;
+  transition-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
+  animation: scaleLogo 450ms ease-in-out;
+  animation-delay: 0.5s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+  margin-bottom: 5%;
+
+
+  .left {
+    transform: translateX(-190%);
+    overflow: visible;
+    animation: xTranslate 450ms ease-in-out;
+    animation-iteration-count: 1;
+    animation-direction: alternate;
+    animation-delay: 0.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+
+  }
+  .right {
+    transform: translateX(190%);
+    overflow: visible;
+    animation: xrTranslate 450ms ease-in-out;
+    animation-iteration-count: 1;
+    animation-direction: alternate;
+    animation-delay: 0.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+
+  }
+  .logo {
+    transform: translateX(250);
+    clip-path: inset(0% 50% 0% 50%);
+    animation: reveal 350ms ease-in-out;
+    animation-delay: 0.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+
+  }
+}
+@keyframes scaleLogo {
+  0% {
+    transform: scale(3) translateY(50%);;
+    transition-timing-function: ease-in-out;
+  }
+  100% {
+    transform: scale(1) translateY(0%);;
+    transition-timing-function: ease-in-out;
+  }
+}
+
+@keyframes xTranslate {
+  0% {
+    transform: translateX(-190%);
+    transition-timing-function: ease-in-out;
+   
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
+@keyframes xrTranslate {
+  0% {
+    transform: translateX(190%);
+    transition-timing-function: ease-in-out;
+    transition-duration: 0.5s;
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
+@keyframes reveal {
+  to {
+    clip-path: inset(0 0 0 0);
+  }
+}
+@keyframes revealb {
+  from{
+    opacity: 0;
+    transform: translateY(100%);
+
+  }
+  to{
+    transform: translateY(0%);
+    visibility: visible;
+    opacity: 1;
+
+  }
+}
+</style>
