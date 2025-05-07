2413ebd95c63baf61d00a043fbb197a82db70450
Joshua Guillen <131779397+jojo66089@users.noreply.github.com>
2025-05-07 13:19:29 -0400

    Update resume.vue
    sizing

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,6 +4,7 @@ const route = useRoute()
 
 </script>
 <template>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <headerTitle></headerTitle>
         <div class="resume-container">
         <!-- Resume Header -->
